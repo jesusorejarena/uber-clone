93f345ba72b186d11367d5f5b21f256fd393d08a
jesusorejarena <jesusorejarena@gmail.com>
2022-01-11 07:46:40 -0400

    Backup

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const Stack = createStackNavigator();
+
+	/* 1:37:29 */
+
+	return (
+		<Provider store={store}>
+			<NavigationContainer>
+				<SafeAreaProvider>
+					<Stack.Navigator>
+						<Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+						<Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false }} />
+					</Stack.Navigator>
+				</SafeAreaProvider>
+			</NavigationContainer>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
